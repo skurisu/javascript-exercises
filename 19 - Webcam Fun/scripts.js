8a900ee4faff6656f0b6a54115ff13b6a0b71768
Sarah Kurisu <sarah.kurisu@gmail.com>
2017-02-03 15:29:51 -0500

    completed webcam

---
+++ 19 - Webcam Fun/scripts.js
@@ -0,0 +1,69 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+  .then(localMediaStream => {
+    console.log(localMediaStream);
+    video.src = window.URL.createObjectURL(localMediaStream); // convert to live video feed
+    video.play();
+  })
+  .catch(err => {
+    console.error(`Allow webcam access`, err);
+  })
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height) // pass image or video element
+    // take pixels out
+    let pixels = ctx.getImageData(0,0,width,height);
+    // adjust values
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
+    // put pixels back
+    ctx.putImageData(pixels, 0, 0);
+  }, 16)
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  // take data from canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="yay" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 150; // red
+    pixels.data[i + 1] = pixels.data[i + 1] - 20; // green
+    pixels.data[i + 2] = pixels.data[i + 2] * .5; // blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i -150] = pixels.data[i + 0]; // red
+    pixels.data[i + 100] = pixels.data[i + 1]; // green
+    pixels.data[i -150] = pixels.data[i + 2]; // blue
+  }
+  return pixels;
+}
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
